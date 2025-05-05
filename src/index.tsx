0feac8d51f92759a0fb9b48e8eb7f071c7c49c2d
shayan khaleghparast <iman@regentmarkets.com>
2025-05-05 16:41:30 +0800

    fix: reverted credentials property

--- src/index.tsx
+++ src/index.tsx
@@ -560,7 +560,7 @@ class UserJourneyTracker {
                 },
                 body: JSON.stringify(payload),
                 // Use credentials to include cookies in cross-origin requests
-                // credentials: 'omit'
+                credentials: 'include'
             });
 
             if (!response.ok) {
